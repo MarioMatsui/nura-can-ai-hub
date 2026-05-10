56efdb25d492fe002eb1cc2ff905c60959de848f
ZenithCraft <helios.it.pro@outlook.com>
2026-05-10 09:23:12 -0400

    upate: optimize preload=none for loading video

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -38,6 +38,7 @@ const HowItWorks = () => {
             onEnded={handleVideoEnd}
             onPause={handleVideoPause}
             onPlay={handleVideoPlay}
+            preload="none"
             playsInline
           />
 
